691c2d4af5d473edb4a91694d4278b77c0a16548
Batfloman <79253255+Batfloman@users.noreply.github.com>
2022-06-06 12:23:05 +0200

    animation popup settings

--- web/src/index.js
+++ web/src/index.js
@@ -14,44 +14,69 @@ const themes = {
 };
 
 let currentTheme = "dark";
-let settingsActive = false;
 
-window.onload = () => {
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const params = Object.fromEntries(urlSearchParams.entries());
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
 
-  if (themes[params.theme]) {
-    currentTheme = params.theme;
-  }
+if (themes[params.theme]) {
+  currentTheme = params.theme;
+}
+updateTheme();
+
+// ==========================================================================================
+// css changes
+const root = document.querySelector(":root");
+const rs = root.style;
+
+// theme style change on button click
+const theme_button = document.getElementById("theme-button");
+theme_button.addEventListener("click", () => {
+  if (currentTheme == "dark") currentTheme = "light";
+  else currentTheme = "dark";
+
   updateTheme();
+});
 
-  // ==========================================================================================
-  // css changes
-  const root = document.querySelector(":root");
-  const rs = root.style;
+// ==========================================================================================
+// show option panel and reverse rotation of button
+var settingsActive = false;
 
-  // theme style change on button click
-  const theme_button = document.getElementById("theme-button");
-  theme_button.addEventListener("click", () => {
-    if (currentTheme == "dark") currentTheme = "light";
-    else currentTheme = "dark";
+const settings_button = document.getElementById("setting-btn");
+const settings_popup = document.getElementById("settings-popup");
 
-    updateTheme();
-  });
+settings_button.onclick = () => {
+  settingsActive ? hideSettingsPopup() : showSettingsPopup();
 
-  // show option panel and reverse rotation of button
-  const settings_button = document.getElementById("setting-btn");
-  settings_button.onclick = () => {
-    if (settingsActive) {
-      rs.setProperty("--settings-hide-popup", "hidden");
-      rs.setProperty("--settings-rotation", "rotate(60deg)");
-    } else {
-      rs.setProperty("--settings-hide-popup", "visible");
-      rs.setProperty("--settings-rotation", "rotate(-60deg)");
-    }
-    settingsActive = !settingsActive;
-  };
+  settingsActive = !settingsActive;
 };
+
+function hideSettingsPopup() {
+  console.log("hide");
+  rs.setProperty("--settings-hide-popup", "hidden");
+  rs.setProperty("--settings-rotation", "rotate(60deg)");
+  settings_popup.classList.add("roll-out-top");
+}
+
+function showSettingsPopup() {
+  console.log("show");
+  rs.setProperty("--settings-hide-popup", "visible");
+  rs.setProperty("--settings-rotation", "rotate(-60deg)");
+  settings_popup.classList.add("roll-in-top");
+}
+
+settings_popup.addEventListener("animationend", (e) => {
+  switch (e.animationName) {
+    case "roll-in-top":
+      settings_popup.classList.remove("roll-in-top");
+      break;
+    case "roll-out-top":
+      settings_popup.classList.remove("roll-out-top");
+      break;
+  }
+  // if(settingsActive) rs.setProperty("--settings-hide-popup", "hidden");
+});
+
+// ==========================================================================================
 
 function updateTheme() {
   const root = document.querySelector(":root");
@@ -70,14 +95,14 @@ function updateTheme() {
     const btn = document.getElementById(btnID);
     const href = btn.getAttribute("href");
 
-    if(!href) return;
+    if (!href) return;
 
     let newHref;
 
-    if(href.match(themePattern)) {
+    if (href.match(themePattern)) {
       newHref = href.replace(themePattern, "theme=" + currentTheme);
     } else {
-      if(href.match(/\?/)) newHref = href + "theme=" + currentTheme;
+      if (href.match(/\?/)) newHref = href + "theme=" + currentTheme;
       else newHref = href + "?theme=" + currentTheme;
     }
 
@@ -93,7 +118,7 @@ function changeVarInUrl(varName, value) {
   if (!urlParams.match(pattern)) urlParams += "theme=" + value;
   else urlParams = urlParams.replace(pattern, "theme=" + value);
 
-  if(window.location.search == urlParams) return;
+  if (window.location.search == urlParams) return;
 
   window.location.search = urlParams;
 }
