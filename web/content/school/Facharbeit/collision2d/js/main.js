b28f2cfebcc543db88586ec3ee99a5ab67cf0f20
Batfloman <79253255+Batfloman@users.noreply.github.com>
2022-06-08 10:41:54 +0200

    moved script in body tag
    removed now unneeded window.onload

--- web/content/school/Facharbeit/collision2d/js/main.js
+++ web/content/school/Facharbeit/collision2d/js/main.js
@@ -15,59 +15,57 @@ const maxRadius = 75;
 const minIrregularity = 1;
 const maxIrregularity = 1;
 var speedBefore = 0;
-window.onload = () => {
-    let s = new System(new Canvas(document.querySelector("canvas")));
-    s.setCamaraMovementLock(false);
-    s.setCamaraScaleLock(false);
-    s.setWorldChunkSize(500);
-    s.getCamara().setMaxZoomOutAmount(12);
-    Input.newEventListener("click", s, () => {
-        const worldPos = Util.position.staticPos_to_worldPos(s.getCamara(), Input.mPosHover);
-        const world = s.getWorld();
-        if (!world)
-            return;
-        const chunkPos = world.findChunkOfPos(worldPos);
-        const chunk = world.getChunk(chunkPos.x, chunkPos.y);
-        if (!chunk)
-            return;
-        const chunks = world.findNeighbourChunksOf(chunk);
-        for (let chunk of chunks) {
-            for (let obj of chunk.findObjects(FormObject)) {
-                const distance = Util.distance(worldPos, obj.pos);
-                if (distance <= selectDistance) {
-                    obj.selected = !obj.selected;
-                    return;
-                }
+let s = new System(new Canvas(document.querySelector("canvas")));
+s.setCamaraMovementLock(false);
+s.setCamaraScaleLock(false);
+s.setWorldChunkSize(500);
+s.getCamara().setMaxZoomOutAmount(12);
+Input.newEventListener("click", s, () => {
+    const worldPos = Util.position.staticPos_to_worldPos(s.getCamara(), Input.mPosHover);
+    const world = s.getWorld();
+    if (!world)
+        return;
+    const chunkPos = world.findChunkOfPos(worldPos);
+    const chunk = world.getChunk(chunkPos.x, chunkPos.y);
+    if (!chunk)
+        return;
+    const chunks = world.findNeighbourChunksOf(chunk);
+    for (let chunk of chunks) {
+        for (let obj of chunk.findObjects(FormObject)) {
+            const distance = Util.distance(worldPos, obj.pos);
+            if (distance <= selectDistance) {
+                obj.selected = !obj.selected;
+                return;
             }
         }
-    });
-    const stopButton = new UISimpleButton(new Vector2(7, 5), "10", "4", "Stop");
-    stopButton.action = () => {
-        let temp = speedBefore;
-        speedBefore = s.speedMult;
-        s.speedMult = temp;
-    };
-    s.addObject(stopButton);
-    const speedUp = new UISimpleButton(new Vector2(7, 13), "10", "4", "Speed Up");
-    speedUp.action = () => {
-        s.speedMult *= 1.25;
-    };
-    s.addObject(speedUp);
-    const slowDown = new UISimpleButton(new Vector2(7, 18), "10", "4", "Slow Down");
-    slowDown.action = () => {
-        s.speedMult /= 1.25;
-    };
-    s.addObject(slowDown);
-    const reverseRotation = new UISimpleButton(new Vector2(7, 23), "10", "4", "reverse Rotation");
-    reverseRotation.action = () => {
-        s.speedMult *= -1;
-    };
-    s.addObject(reverseRotation);
-    for (let i = 0; i < amountShapes; i++) {
-        s.addObject(createRandomShape());
     }
-    s.start();
+});
+const stopButton = new UISimpleButton(new Vector2(7, 5), "10", "4", "Stop");
+stopButton.action = () => {
+    let temp = speedBefore;
+    speedBefore = s.speedMult;
+    s.speedMult = temp;
 };
+s.addObject(stopButton);
+const speedUp = new UISimpleButton(new Vector2(7, 13), "10", "4", "Speed Up");
+speedUp.action = () => {
+    s.speedMult *= 1.25;
+};
+s.addObject(speedUp);
+const slowDown = new UISimpleButton(new Vector2(7, 18), "10", "4", "Slow Down");
+slowDown.action = () => {
+    s.speedMult /= 1.25;
+};
+s.addObject(slowDown);
+const reverseRotation = new UISimpleButton(new Vector2(7, 23), "10", "4", "reverse Rotation");
+reverseRotation.action = () => {
+    s.speedMult *= -1;
+};
+s.addObject(reverseRotation);
+for (let i = 0; i < amountShapes; i++) {
+    s.addObject(createRandomShape());
+}
+s.start();
 function createRandomShape() {
     let start = new Vector2(Util.math.random.between(-fieldSize / 2, fieldSize / 2, 2), Util.math.random.between(-fieldSize / 2, fieldSize / 2, 2));
     let form = new Form(Util.math.random.between(minVertices, maxVertices), Util.math.random.between(minRadius, maxRadius, 2), Util.math.random.between(minIrregularity, maxIrregularity, 2));
--- web/content/school/Facharbeit/collision2d/ts/main.ts
+++ web/content/school/Facharbeit/collision2d/ts/main.ts
@@ -23,120 +23,118 @@ const maxIrregularity = 1;
 
 var speedBefore = 0;
 
-window.onload = () => {
-  let s = new System(new Canvas(document.querySelector("canvas")));
+let s = new System(new Canvas(document.querySelector("canvas")));
 
-  // ==========================================================================================
-  // #region settings
+// ==========================================================================================
+// #region settings
 
-  s.setCamaraMovementLock(false);
-  s.setCamaraScaleLock(false);
-  s.setWorldChunkSize(500);
+s.setCamaraMovementLock(false);
+s.setCamaraScaleLock(false);
+s.setWorldChunkSize(500);
 
-  s.getCamara().setMaxZoomOutAmount(12);
+s.getCamara().setMaxZoomOutAmount(12);
 
-  //#endregion
+//#endregion
 
-  // ==========================================================================================
-  // #region UI
+// ==========================================================================================
+// #region UI
 
-  // select Objects on click
-  Input.newEventListener("click", s, () => {
-    const worldPos = Util.position.staticPos_to_worldPos(s.getCamara(), Input.mPosHover);
+// select Objects on click
+Input.newEventListener("click", s, () => {
+  const worldPos = Util.position.staticPos_to_worldPos(s.getCamara(), Input.mPosHover);
 
-    const world = s.getWorld();
-    if (!world) return;
+  const world = s.getWorld();
+  if (!world) return;
 
-    const chunkPos = world.findChunkOfPos(worldPos);
-    const chunk = world.getChunk(chunkPos.x, chunkPos.y);
-    if (!chunk) return;
-    const chunks = world.findNeighbourChunksOf(chunk);
+  const chunkPos = world.findChunkOfPos(worldPos);
+  const chunk = world.getChunk(chunkPos.x, chunkPos.y);
+  if (!chunk) return;
+  const chunks = world.findNeighbourChunksOf(chunk);
 
-    for (let chunk of chunks) {
-      for (let obj of chunk.findObjects<FormObject>(FormObject)) {
-        const distance = Util.distance(worldPos, obj.pos);
+  for (let chunk of chunks) {
+    for (let obj of chunk.findObjects<FormObject>(FormObject)) {
+      const distance = Util.distance(worldPos, obj.pos);
 
-        if (distance <= selectDistance) {
-          obj.selected = !obj.selected;
-          return; // select only one at a time!
-        }
+      if (distance <= selectDistance) {
+        obj.selected = !obj.selected;
+        return; // select only one at a time!
       }
     }
-  });
-
-  // stop game
-  const stopButton = new UISimpleButton(new Vector2(7, 5), "10", "4", "Stop");
-  stopButton.action = () => {
-    let temp = speedBefore;
-    speedBefore = s.speedMult;
-    s.speedMult = temp;
-  };
-  s.addObject(stopButton);
-
-  // speed up Object rotation
-  const speedUp = new UISimpleButton(new Vector2(7, 13), "10", "4", "Speed Up");
-  speedUp.action = () => {
-    s.speedMult *= 1.25;
-  };
-  s.addObject(speedUp);
-
-  // slow down Object rotation
-  const slowDown = new UISimpleButton(new Vector2(7, 18), "10", "4", "Slow Down");
-  slowDown.action = () => {
-    s.speedMult /= 1.25;
-  };
-  s.addObject(slowDown);
-
-  // reverse Object rotation
-  const reverseRotation = new UISimpleButton(new Vector2(7, 23), "10", "4", "reverse Rotation");
-  reverseRotation.action = () => {
-    s.speedMult *= -1;
-  };
-  s.addObject(reverseRotation);
-
-  //#endregion
-
-  // ==========================================================================================
-  // #region contents
-
-  for (let i = 0; i < amountShapes; i++) {
-    s.addObject(createRandomShape());
   }
+});
 
-  // let form = new FormObject(
-  //   new Vector2(-100, 0),
-  //   new Polygon2([
-  //     new Vector2(0, 100),
-  //     new Vector2(200, 120),
-  //     new Vector2(10, 0),
-  //     new Vector2(200, -120),
-  //     new Vector2(0, -100),
-  //     new Vector2(-100, 0),
-  //   ]),
-  //   0
-  // );
-  // form.rotationSpeed = 0;
-  // s.addObject(form);
-
-  // let form2 = new FormObject(
-  //   new Vector2(100, 0),
-  //   new Polygon2([
-  //     new Vector2(0, 100),
-  //     new Vector2(200, 120),
-  //     new Vector2(10, 0),
-  //     new Vector2(200, -120),
-  //     new Vector2(0, -100),
-  //     new Vector2(-100, 0),
-  //   ])
-  // )
-  // form2.selected = true;
-  // form2.rotationSpeed = 0;
-  // s.addObject(form2);
-
-  //#endregion
-
-  s.start();
+// stop game
+const stopButton = new UISimpleButton(new Vector2(7, 5), "10", "4", "Stop");
+stopButton.action = () => {
+  let temp = speedBefore;
+  speedBefore = s.speedMult;
+  s.speedMult = temp;
 };
+s.addObject(stopButton);
+
+// speed up Object rotation
+const speedUp = new UISimpleButton(new Vector2(7, 13), "10", "4", "Speed Up");
+speedUp.action = () => {
+  s.speedMult *= 1.25;
+};
+s.addObject(speedUp);
+
+// slow down Object rotation
+const slowDown = new UISimpleButton(new Vector2(7, 18), "10", "4", "Slow Down");
+slowDown.action = () => {
+  s.speedMult /= 1.25;
+};
+s.addObject(slowDown);
+
+// reverse Object rotation
+const reverseRotation = new UISimpleButton(new Vector2(7, 23), "10", "4", "reverse Rotation");
+reverseRotation.action = () => {
+  s.speedMult *= -1;
+};
+s.addObject(reverseRotation);
+
+//#endregion
+
+// ==========================================================================================
+// #region contents
+
+for (let i = 0; i < amountShapes; i++) {
+  s.addObject(createRandomShape());
+}
+
+// let form = new FormObject(
+//   new Vector2(-100, 0),
+//   new Polygon2([
+//     new Vector2(0, 100),
+//     new Vector2(200, 120),
+//     new Vector2(10, 0),
+//     new Vector2(200, -120),
+//     new Vector2(0, -100),
+//     new Vector2(-100, 0),
+//   ]),
+//   0
+// );
+// form.rotationSpeed = 0;
+// s.addObject(form);
+
+// let form2 = new FormObject(
+//   new Vector2(100, 0),
+//   new Polygon2([
+//     new Vector2(0, 100),
+//     new Vector2(200, 120),
+//     new Vector2(10, 0),
+//     new Vector2(200, -120),
+//     new Vector2(0, -100),
+//     new Vector2(-100, 0),
+//   ])
+// )
+// form2.selected = true;
+// form2.rotationSpeed = 0;
+// s.addObject(form2);
+
+//#endregion
+
+s.start();
 
 function createRandomShape(): FormObject {
   let start = new Vector2(
